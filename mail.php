dc60259c94210eafe8aae4decdcec7ecf1bf53bf
tongniu2 <you@example.com>
2016-12-25 01:58:27 -0500

    Request to Post

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
     $to = "ntong283@gmail.com";
-    $from = $_REQUEST['name'];
+    $from = $_POST['name'];
     $headers = "Content-type: text/html;From: $from";
 
     $fields = array();
-    $fields["name"] = $_REQUEST['name'];
-    $fields["email"] = $_REQUEST['email'];
-    $fields["message"] = $_REQUEST['message'];
+    $fields["name"] = $_POST['name'];
+    $fields["email"] = $_POST['email'];
+    $fields["message"] = $_POST['message'];
 
     $body = "Here is what was sent:\n\n";
     $body .= 'Name : '.$fields['name']. '<br>';
