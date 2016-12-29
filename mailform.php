43b8d38b262d1e55b5939370dbf22b14b728f6ed
tongniu2 <tong.niu2@mail.mcgill.ca>
2016-12-28 23:32:32 -0500

    Update mailform.php

--- mailform.php
+++ mailform.php
@@ -2,11 +2,11 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-  $email_from = 'yourname@yourwebsite.com';
+  $email_from = 'ntong283@gmail.com';
   $email_subject = "New Form submission";
   $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
-  $to = "yourname@yourwebsite.com";
+  $to = "ntong283@gmail.com";
   $headers = "From: $email_from \r\n";
   $headers .= "Reply-To: $visitor_email \r\n";
   mail($to,$email_subject,$email_body,$headers);
